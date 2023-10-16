84fce3b7c1ecbd8d4d06829fa0bbea01663e20da
Steven <quezadasteven570@gmail.com>
2023-10-16 00:05:28 -0600

    Pnatalla inicio admin y activity para hacer el menu

---
+++ app/src/main/java/com/example/bravocollect/InicioAdministrador.java
@@ -0,0 +1,14 @@
+package com.example.bravocollect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicioAdministrador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_administrador);
+    }
+}
