eec4b744dd6482ee78fbf639f89bc3b368160212
Oscar <oscarbarcenas417@gmail.com>
2023-10-18 07:29:44 -0600

    Commit login & sign in

--- app/src/main/java/com/example/bravocollect/MainActivity.java
+++ app/src/main/java/com/example/bravocollect/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.bravocollect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Ola
         //soy joto
+    }
+
+    public void login (View view){
+        Intent ini_sesion = new Intent(this, InicioAdministrador.class);
+        startActivity(ini_sesion);
+    }
+    public void signin (View view){
+        Intent ini_sesion = new Intent(this, InicioAdministrador.class);
+        startActivity(ini_sesion);
     }
 }
