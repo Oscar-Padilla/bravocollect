3e05eeebe92b27ed75a0b1a5afcf09582031d292
Oscar <oscarbarcenas417@gmail.com>
2023-11-05 23:10:52 -0600

    Commit de pantallas ya modificadas

--- app/src/main/java/com/example/bravocollect/MainActivity.java
+++ app/src/main/java/com/example/bravocollect/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Ola
+
     }
     public void login (View view){
         Intent ini_sesion = new Intent(this, InicioAdministrador.class);
