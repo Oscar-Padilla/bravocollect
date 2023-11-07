a9b4e373a6ff07b5acfcf79fa07b317d1978b195
Steven <quezadasteven570@gmail.com>
2023-11-06 20:56:06 -0600

    Manda parametros a otra pantalla.

--- app/src/main/java/com/example/bravocollect/InicioAdministrador.java
+++ app/src/main/java/com/example/bravocollect/InicioAdministrador.java
@@ -3,9 +3,12 @@ package com.example.bravocollect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class InicioAdministrador extends AppCompatActivity {
+
+    private EditText et1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +16,9 @@ public class InicioAdministrador extends AppCompatActivity {
         setContentView(R.layout.activity_inicio_administrador);
         Toast.makeText(this, "Inicio de sesion exitoso, Bienvenido", Toast.LENGTH_LONG).show();
 
+        et1 = (EditText) findViewById(R.id.editText_buscar);
 
-
-
+        String dato = getIntent().getStringExtra("dato");
+        et1.setText("Hola "+dato);
     }
 }
--- app/src/main/java/com/example/bravocollect/MainActivity.java
+++ app/src/main/java/com/example/bravocollect/MainActivity.java
@@ -3,21 +3,29 @@ package com.example.bravocollect;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
+private EditText et1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Ola
+        et1 = (EditText)findViewById(R.id.editText_correo);
 
     }
     public void login (View view){
         Intent ini_sesion = new Intent(this, InicioAdministrador.class);
+        ini_sesion.putExtra("dato",et1.getText().toString());
+
         startActivity(ini_sesion);
+
     }
+
+
+
 }
