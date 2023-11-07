efa2bcfc9ef2cafe474e92589b17978a163d95e3
Steven <quezadasteven570@gmail.com>
2023-11-06 20:14:32 -0600

    Toast cuando se inicia sesion.

--- app/src/main/java/com/example/bravocollect/InicioAdministrador.java
+++ app/src/main/java/com/example/bravocollect/InicioAdministrador.java
@@ -3,6 +3,7 @@ package com.example.bravocollect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class InicioAdministrador extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class InicioAdministrador extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_administrador);
+        Toast.makeText(this, "Inicio de sesion exitoso, Bienvenido", Toast.LENGTH_LONG).show();
+
+
+
+
     }
 }
