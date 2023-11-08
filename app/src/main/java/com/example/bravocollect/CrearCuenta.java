19c9f8f817030a4acff192d2c20325ba341c2e56
Oscar <oscarbarcenas417@gmail.com>
2023-11-07 22:00:45 -0600

    Commit final ahora si final final ahora si final

--- app/src/main/java/com/example/bravocollect/CrearCuenta.java
+++ app/src/main/java/com/example/bravocollect/CrearCuenta.java
@@ -2,7 +2,9 @@ package com.example.bravocollect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CrearCuenta extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class CrearCuenta extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_cuenta);
+    }
+    public void crear (View view) {
+        Intent ini = new Intent(this, MainActivity.class);
+        startActivity(ini);
     }
 }
